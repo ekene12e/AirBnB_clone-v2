67e5634862ee954c48a12b92b338c706f2865698
Ekene Ezema <ekenestanekeson@gmail.com>
2022-09-12 21:39:25 +0100

    task 6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
+from models.city import City
 from models.base_model import BaseModel
+from sqlalchemy import Column,  String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
-    """ State class """
-    name = ""
+    """Representation of state """
+    if  models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
