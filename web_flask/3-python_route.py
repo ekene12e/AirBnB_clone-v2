17fd7a43f9f82e087c28c6e16f68887fb49e33b2
ekene12e <ekenestanekeson@gmail.com>
2022-10-28 17:58:34 +0100

    task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Flask framework
+    """
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = 'C ' + text.replace('_', ' ')
+    return text
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    text = 'python ' + text.replace('_', ' ')
+    return text
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
