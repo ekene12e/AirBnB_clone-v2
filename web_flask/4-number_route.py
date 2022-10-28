01e096795809f8dac07505395d58cb40f8bf820b
ekene12e <ekenestanekeson@gmail.com>
2022-10-28 18:10:44 +0100

    task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """ Display Hello HBNB"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ Display HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """ Display C"""
     text = 'C ' + text.replace('_', ' ')
     return text
 
@@ -26,6 +29,7 @@ def c(text):
 @app.route('/python/', defaults={'text': 'is_cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
+    """display python"""
     text = 'Python ' + text.replace('_', ' ')
     return text
 
