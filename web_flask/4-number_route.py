795b830fbe52af2eb2ff70a0c9db250ab5926854
ekene12e <ekenestanekeson@gmail.com>
2022-10-28 18:06:01 +0100

    task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Flask framework
+    """
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = 'C ' + text.replace('_', ' ')
+    return text
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    text = 'Python ' + text.replace('_', ' ')
+    return text
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    text = f'{n} is a number'
+    return text
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
